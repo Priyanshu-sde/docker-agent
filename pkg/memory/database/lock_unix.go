9a985dcbf82d7125468ddd0aa61018f78f43d73c
yunasora <yunasora.ink@gmail.com>
2026-07-03 18:29:02 +0800

    fix(memory): address write serialization review feedback

--- pkg/memory/database/lock_unix.go
+++ pkg/memory/database/lock_unix.go
@@ -4,7 +4,6 @@ package database
 
 import (
 	"errors"
-	"io"
 	"os"
 
 	"golang.org/x/sys/unix"
@@ -13,25 +12,13 @@ import (
 // lockFileExclusive attempts to acquire an exclusive advisory lock without
 // blocking. The retry loop in FileLock.Lock handles waiting and cancellation.
 func lockFileExclusive(f *os.File) error {
-	lock := unix.Flock_t{
-		Type:   unix.F_WRLCK,
-		Whence: int16(io.SeekStart),
-		Start:  0,
-		Len:    0,
-	}
-	return unix.FcntlFlock(f.Fd(), unix.F_SETLK, &lock)
+	return unix.Flock(int(f.Fd()), unix.LOCK_EX|unix.LOCK_NB)
 }
 
 func unlockFile(f *os.File) error {
-	lock := unix.Flock_t{
-		Type:   unix.F_UNLCK,
-		Whence: int16(io.SeekStart),
-		Start:  0,
-		Len:    0,
-	}
-	return unix.FcntlFlock(f.Fd(), unix.F_SETLK, &lock)
+	return unix.Flock(int(f.Fd()), unix.LOCK_UN)
 }
 
 func isLockUnavailable(err error) bool {
-	return errors.Is(err, unix.EACCES) || errors.Is(err, unix.EAGAIN)
+	return errors.Is(err, unix.EWOULDBLOCK) || errors.Is(err, unix.EAGAIN)
 }
